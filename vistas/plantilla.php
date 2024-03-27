4a0c99c8dc3d89f7d422b2add9409ac1945bec7f
jamtzmancilla <jamtzmancilla@gmail.com>
2024-03-26 22:51:06 -0600

    plantilla
    update plantilla

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sistema Personal</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Personal</title>
 </head>
 
 <body>
@@ -14,12 +15,13 @@
     
     if (isset($_GET["ruta"])) {
 
-      if ($_GET["ruta"]=="inicio"||
-             $_GET["ruta"]=="perfil"||
-             $_GET["ruta"]=="productos"){
+      if ($_GET["ruta"] =="inicio"||
+             $_GET["ruta"] =="perfil"||
+             $_GET["ruta"] =="productos"){
         include "modulos/" .$_GET["ruta"]. ".php";
     }
   }
              ?>
 </body>
+
 </html>
